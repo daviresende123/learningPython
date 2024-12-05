8ed885c563628de556c8e3cd56b7588fe773f6c5
Davi Resende <davi.resende@estudante.ufla.br>
2024-12-05 13:18:44 -0300

    Just the basics of Python programming

---
+++ Introduction_I/numeros.py
@@ -0,0 +1,7 @@
+palavra1 = "Salve "
+nome = "Davi "
+palavra2 = "tenho "
+idade = 23
+palavra3 = " anos"
+
+print(palavra1 + nome + palavra2 + str(idade) + palavra3)
---
+++ Introduction_I/strings.py
@@ -0,0 +1,5 @@
+nome1 = "davi"
+nome2 = "gomides"
+nome_completo = nome1 + " " + nome2
+
+print("Ola " + nome_completo.title() + ", suave com vc meu amigo?" + "\nQuanto ficou o jogo do Cruzeiro ontem?")
